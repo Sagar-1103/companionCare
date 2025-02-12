84e134623005cfce0f93a8ffa4d57f69c8a64354
ved_shetye <vedbshetye03@gmail.com>
2025-02-12 23:37:05 +0530

    minor changes

--- screens/common/MedicationReminder.jsx
+++ screens/common/MedicationReminder.jsx
@@ -35,6 +35,9 @@ const MedicationReminder = () => {
   const [toDate, setToDate] = useState(null);
   const [isFromDatePickerOpen, setIsFromDatePickerOpen] = useState(false);
   const [isToDatePickerOpen, setIsToDatePickerOpen] = useState(false);
+  const [isBeforeMeal, setIsBeforeMeal] = useState(false);
+  const [isAfterMeal, setIsAfterMeal] = useState(false);
+  const [time, setTime] = useState('');
 
   const handleMealSelection = (meal) => {
     setSelectedMeals((prev) =>
@@ -45,7 +48,17 @@ const MedicationReminder = () => {
   const formatDate = (date) => (date ? date.toLocaleDateString() : '');
 
   const handleSubmit = () => {
-    console.log({ selectedMeals, medicineType, medicineName, dosageUnit, fromDate, toDate });
+    console.log({
+      selectedMeals,
+      medicineType,
+      medicineName,
+      dosageUnit,
+      fromDate,
+      toDate,
+      isBeforeMeal,
+      isAfterMeal,
+      time,
+    });
   };
 
   return (
@@ -54,130 +67,179 @@ const MedicationReminder = () => {
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView contentContainerStyle={[styles.container, { backgroundColor: '#fff' }]}>
-          <View style={[styles.header, { backgroundColor: 'rgb(111, 162, 189)' }]}>
-            <Image
-              source={require('../../assets/medBg.png')}
-              style={styles.headerImage}
-            />
-          </View>
+        <View style={{ flex: 1 }}>
+          <ScrollView
+            contentContainerStyle={[styles.container, { paddingBottom: 150 }]} 
+          >
+            <View style={[styles.header, { backgroundColor: 'rgb(97, 142, 166)' }]}>
+              <Image
+                source={require('../assets/medBg.png')}
+                style={styles.headerImage}
+                resizeMode="contain" 
+              />
+            </View>
 
-          <View style={styles.mealSelection}>
-            {meals.map((meal, index) => {
-              const isSelected = selectedMeals.includes(meal.name);
-              return (
-                <TouchableOpacity
-                  key={index}
-                  style={styles.mealButton}
-                  onPress={() => handleMealSelection(meal.name)}
-                >
-                  <meal.type
-                    name={meal.icon}
-                    size={38}
-                    color={isSelected ? '#003366' : '#AAA'}
-                  />
-                  <Text
-                    style={[styles.mealText, isSelected && { color: '#003366' }]}
+            <View style={styles.mealSelection}>
+              {meals.map((meal, index) => {
+                const isSelected = selectedMeals.includes(meal.name);
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    style={styles.mealButton}
+                    onPress={() => handleMealSelection(meal.name)}
                   >
-                    {meal.name}
+                    <meal.type
+                      name={meal.icon}
+                      size={38}
+                      color={isSelected ? '#003366' : '#AAA'}
+                    />
+                    <Text
+                      style={[styles.mealText, isSelected && { color: '#003366' }]}
+                    >
+                      {meal.name}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+
+            <View style={styles.form}>
+              <View style={[styles.inputGroup, { flexDirection: 'row', alignItems: 'center', backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: '2%'}]}>
+                  <MaterialCommunityIcons name="medical-bag" size={35} color="#888" />
+                  <TextInput
+                      placeholder="Medicine Name"
+                      style={{ flex: 1, paddingVertical: 12, paddingLeft: 11, color: '#000' , fontSize: 19}}
+                      value={medicineName}
+                      onChangeText={setMedicineName}
+                      placeholderTextColor={'#888'}
+                  />
+              </View>
+
+              <View style={styles.row}>
+                <View style={[styles.dropdown, { flexDirection: 'row', alignItems: 'center', paddingLeft: '3%', backgroundColor: '#e8e8e8', borderRadius: 10 }]}>
+                  <FontAwesome5 name="notes-medical" size={32} color="#888" />
+                  <Picker
+                      selectedValue={medicineType}
+                      onValueChange={(itemValue) => setMedicineType(itemValue)}
+                      style={{ flex: 1, color: medicineType ? '#000' : '#888', paddingLeft:'6%' }}
+                  >
+                      <Picker.Item style={{ fontSize: 19 }} label="Type..." value="" enabled={true} />
+                      <Picker.Item style={{ fontSize: 19 }} label="Pills" value="Pills" />
+                      <Picker.Item style={{ fontSize: 19 }} label="Syrup" value="Syrup" />
+                      <Picker.Item style={{ fontSize: 19 }} label="Injection" value="Injection" />
+                  </Picker>
+                </View>
+                <View style={[styles.dropdownleft, { flexDirection: 'row', alignItems: 'center', paddingHorizontal: '4%', paddingVertical: '-1%', backgroundColor: '#e8e8e8', borderRadius: 10 }]}>
+                  <FontAwesome5 name="stethoscope" size={30} color="#888" />
+                  <TextInput
+                      placeholder="Dosage"
+                      style={{ flex: 1, backgroundColor: '#e8e8e8', color: '#000', fontSize: 19, paddingLeft: 13 }}
+                      value={dosageUnit}
+                      onChangeText={setDosageUnit}
+                      placeholderTextColor={'#888'}
+                  />
+                </View>
+              </View>
+
+              <View style={[styles.checkboxGroup, { backgroundColor: '#e8e8e8', borderRadius: 10, padding: '3%' }]}>
+                <View style={styles.row}>
+                  <View style={[styles.checkboxContainer, { flexDirection: 'row', alignItems: 'center' }]}>
+                    <TouchableOpacity
+                      style={[styles.checkbox, isBeforeMeal && { backgroundColor: '#003366' }]}
+                      onPress={() => {
+                        setIsBeforeMeal(!isBeforeMeal);
+                        if (isAfterMeal) setIsAfterMeal(false);
+                      }}
+                    >
+                      {isBeforeMeal && <Ionicons name="checkmark" size={20} color="#fff" />}
+                    </TouchableOpacity>
+                    <Text style={styles.checkboxLabel}>Before Meal</Text>
+                  </View>
+                  <View style={[styles.checkboxContainer, { flexDirection: 'row', alignItems: 'center' }]}>
+                    <TouchableOpacity
+                      style={[styles.checkbox, isAfterMeal && { backgroundColor: '#003366' }]}
+                      onPress={() => {
+                        setIsAfterMeal(!isAfterMeal);
+                        if (isBeforeMeal) setIsBeforeMeal(false);
+                      }}
+                    >
+                      {isAfterMeal && <Ionicons name="checkmark" size={20} color="#fff" />}
+                    </TouchableOpacity>
+                    <Text style={styles.checkboxLabel}>After Meal</Text>
+                  </View>
+                </View>
+              </View>
+
+              {(isBeforeMeal || isAfterMeal) && (
+                <View style={[styles.inputGroup, { flexDirection: 'row', alignItems: 'center', backgroundColor: '#e8e8e8', borderRadius: 10, paddingHorizontal: '2%' }]}>
+                  <Ionicons name="time" size={30} color="#888" />
+                  <TextInput
+                    placeholder="Time (in minutes)"
+                    style={{ flex: 1, paddingVertical: 12, paddingLeft: 11, color: '#000', fontSize: 19 }}
+                    value={time}
+                    onChangeText={setTime}
+                    placeholderTextColor={'#888'}
+                    keyboardType="numeric"
+                  />
+                </View>
+              )}
+
+              <View style={styles.row}>
+                <TouchableOpacity
+                  onPress={() => setIsFromDatePickerOpen(true)}
+                  style={[styles.input, styles.dateInput, { backgroundColor: '#e8e8e8' }]}
+                >
+                  <Ionicons name="calendar" size={30} color="#888" paddingRight='9%'/>
+                  <Text style={{ color: fromDate ? '#000' : '#888', fontSize: 19 }}>
+                    {fromDate ? formatDate(fromDate) : 'From'}
                   </Text>
                 </TouchableOpacity>
-              );
-            })}
-          </View>
-
-          <View style={styles.form}>
-            <View style={[styles.inputGroup, { flexDirection: 'row', alignItems: 'center', backgroundColor: '#F5F5F5', borderRadius: 10, paddingHorizontal: '2%'}]}>
-                <MaterialCommunityIcons name="medical-bag" size={35} color="#888" />
-                <TextInput
-                    placeholder="Medicine Name"
-                    style={{ flex: 1, paddingVertical: 12, paddingLeft: 11, color: '#000' , fontSize: 19}}
-                    value={medicineName}
-                    onChangeText={setMedicineName}
-                    placeholderTextColor={'#888'}
-                />
-            </View>
-
-            <View style={styles.row}>
-              <View style={[styles.dropdown, { flexDirection: 'row', alignItems: 'center', paddingLeft: '3%', backgroundColor: '#F5F5F5', borderRadius: 10 }]}>
-                <FontAwesome5 name="notes-medical" size={32} color="#888" />
-                <Picker
-                    selectedValue={medicineType}
-                    onValueChange={(itemValue) => setMedicineType(itemValue)}
-                    style={{ flex: 1, color: medicineType ? '#000' : '#888', paddingLeft:'6%' }}
+                <TouchableOpacity
+                  onPress={() => setIsToDatePickerOpen(true)}
+                  style={[styles.input, styles.dateInput, { backgroundColor: '#e8e8e8' }]}
                 >
-                    <Picker.Item style={{ fontSize: 19 }} label="Type..." value="" enabled={true} />
-                    <Picker.Item style={{ fontSize: 19 }} label="Pills" value="Pills" />
-                    <Picker.Item style={{ fontSize: 19 }} label="Syrup" value="Syrup" />
-                    <Picker.Item style={{ fontSize: 19 }} label="Injection" value="Injection" />
-                </Picker>
-              </View>
-              <View style={[styles.dropdownleft, { flexDirection: 'row', alignItems: 'center', paddingHorizontal: '4%', paddingVertical: '-1%', backgroundColor: '#F5F5F5', borderRadius: 10 }]}>
-                <FontAwesome5 name="stethoscope" size={30} color="#888" />
-                <TextInput
-                    placeholder="Dosage"
-                    style={{ flex: 1, backgroundColor: '#F5F5F5', color: '#000', fontSize: 19, paddingLeft: 13 }}
-                    value={dosageUnit}
-                    onChangeText={setDosageUnit}
-                    placeholderTextColor={'#888'}
-                />
+                  <Ionicons name="calendar" size={30} color="#888" paddingRight='9%'/>
+                  <Text style={{ color: toDate ? '#000' : '#888', fontSize: 19 }}>
+                    {toDate ? formatDate(toDate) : 'To'}
+                  </Text>
+                </TouchableOpacity>
               </View>
             </View>
-            <View style={styles.row}>
-              {/* From Date */}
-              <TouchableOpacity
-                onPress={() => setIsFromDatePickerOpen(true)}
-                style={[styles.input, styles.dateInput, { backgroundColor: '#F5F5F5' }]}
-              >
-                <Ionicons name="calendar" size={30} color="#888" paddingRight='9%'/>
-                <Text style={{ color: fromDate ? '#000' : '#888', fontSize: 19 }}>
-                  {fromDate ? formatDate(fromDate) : 'From'}
-                </Text>
-              </TouchableOpacity>
-              {/* To Date */}
-              <TouchableOpacity
-                onPress={() => setIsToDatePickerOpen(true)}
-                style={[styles.input, styles.dateInput, { backgroundColor: '#F5F5F5' }]}
-              >
-                <Ionicons name="calendar" size={30} color="#888" paddingRight='9%'/>
-                <Text style={{ color: toDate ? '#000' : '#888', fontSize: 19 }}>
-                  {fromDate ? formatDate(toDate) : 'To'}
-                </Text>
-              </TouchableOpacity>
-            </View>
+          </ScrollView>
+
+          <View style={styles.floatingButtonContainer}>
+            <TouchableOpacity
+              style={[styles.reminderButton, { backgroundColor: '#003366' }]}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.reminderButtonText}>Set Reminder</Text>
+            </TouchableOpacity>
           </View>
-
-          <TouchableOpacity
-            style={[styles.reminderButton, { backgroundColor: '#003366' }]}
-            onPress={handleSubmit}
-          >
-            <Text style={styles.reminderButtonText}>Set Reminder</Text>
-          </TouchableOpacity>
-
-          <DatePicker
-            modal
-            open={isFromDatePickerOpen}
-            date={fromDate || new Date()}
-            mode="date"
-            onConfirm={(date) => {
-              setIsFromDatePickerOpen(false);
-              setFromDate(date);
-            }}
-            onCancel={() => setIsFromDatePickerOpen(false)}
-          />
-          <DatePicker
-            modal
-            open={isToDatePickerOpen}
-            date={toDate || new Date()}
-            mode="date"
-            onConfirm={(date) => {
-              setIsToDatePickerOpen(false);
-              setToDate(date);
-            }}
-            onCancel={() => setIsToDatePickerOpen(false)}
-          />
-        </ScrollView>
+        </View>
       </TouchableWithoutFeedback>
+
+      <DatePicker
+        modal
+        open={isFromDatePickerOpen}
+        date={fromDate || new Date()}
+        mode="date"
+        onConfirm={(date) => {
+          setIsFromDatePickerOpen(false);
+          setFromDate(date);
+        }}
+        onCancel={() => setIsFromDatePickerOpen(false)}
+      />
+      <DatePicker
+        modal
+        open={isToDatePickerOpen}
+        date={toDate || new Date()}
+        mode="date"
+        onConfirm={(date) => {
+          setIsToDatePickerOpen(false);
+          setToDate(date);
+        }}
+        onCancel={() => setIsToDatePickerOpen(false)}
+      />
     </KeyboardAvoidingView>
   );
 };
@@ -197,7 +259,7 @@ const styles = StyleSheet.create({
   headerImage: {
     width: '85%',
     height: '92%',
-    marginTop: '5%'
+    marginTop: '5%',
   },
   mealSelection: {
     flexDirection: 'row',
@@ -221,7 +283,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: '7%',
   },
   inputGroup: {
-    marginBottom: '8%',
+    marginBottom: '4%',
   },
   row: {
     flexDirection: 'row',
@@ -249,21 +311,53 @@ const styles = StyleSheet.create({
     marginLeft: '3%',
   },
   dateInput: {
-  flex: 0.48,
-  flexDirection: 'row', 
-  alignItems: 'center',   
-  paddingVertical: '4%',
-  paddingHorizontal: '4%',
-  borderRadius: 10,
-  fontSize: 14,
-},
+    flex: 0.48,
+    flexDirection: 'row', 
+    alignItems: 'center',   
+    paddingVertical: '4%',
+    paddingHorizontal: '4%',
+    borderRadius: 10,
+    fontSize: 14,
+  },
+  checkboxGroup: {
+    marginBottom: '4%',
+    marginTop: '-3%',
+  },
+  checkboxContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: '-5%',
+    marginLeft: '6%',
+    marginTop: '2%',
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#888',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  checkboxLabel: {
+    fontSize: 18,
+    color: '#888',
+  },
+  floatingButtonContainer: {
+    position: 'absolute',
+    bottom: '9%',
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
   reminderButton: {
     width: '90%',
     paddingVertical: '4.5%',
     borderRadius: 45,
     alignSelf: 'center',
     alignItems: 'center',
-    marginTop: '1%',
   },
   reminderButtonText: {
     color: '#fff',
